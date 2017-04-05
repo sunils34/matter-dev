4bb64316a58d9959c15d09ec32236f6b98e0318e
Sunil Sadasivan <sunil@matterapp.io>
2017-04-05 10:17:30 -0700

    Support running matter marketing through matter-dev cli

--- cli/matter-dev-env.js
+++ cli/matter-dev-env.js
@@ -20,8 +20,12 @@ function addservice(command, service) {
 
 var MatterCli = {
   'build'(service) {
-    execute(addservice('docker-compose build --no-cache', service));
-    execute('cd ./matter-app && npm install');
+    if (service === 'marketing-local') {
+      execute('cd ./matter-marketing && npm install');
+    } else {
+      execute(addservice('docker-compose build --no-cache', service));
+      execute('cd ./matter-app && npm install');
+    }
   },
   'clean'(service) {
     execute('rm -rf ./data');
@@ -32,13 +36,17 @@ var MatterCli = {
     execute('docker-compose ps');
   },
   'start'(service) {
-    logger.info('Starting dev environment');
-    const execCommand = 'docker-compose up -d';
-    execute(addservice(execCommand, service));
-    execute('docker-compose ps');
-    logger.info('Waiting 10 seconds for the database to start');
-    execute('sleep 10');
-    execute('cd ./matter-app && npm run dev');
+    if (service === 'marketing-local') {
+      execute('cd ./matter-marketing && npm run start-dev');
+    } else {
+      logger.info('Starting dev environment');
+      const execCommand = 'docker-compose up -d';
+      execute(addservice(execCommand, service));
+      execute('docker-compose ps');
+      logger.info('Waiting 10 seconds for the database to start');
+      execute('sleep 10');
+      execute('cd ./matter-app && npm run dev');
+    }
   },
   'stop'(service) {
     const execCommand = 'docker-compose stop';
